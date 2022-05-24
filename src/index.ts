3ac6f593f05f779bf8454e558107d3ca8f2c4516
Taiwo Maja <taiwom@klluster.com>
2022-05-24 12:36:32 +0300

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-import http from 'http'
-import { config } from './config/config'
+import http from 'http';
+import { config } from './config/config';
+import Logger from './logger';
 
 const app = express();
 
 const initServer = () => {
-	http.createServer(app).listen(config.server.port, () => console.log(`Server started`))
-}
+	http
+		.createServer(app)
+		.listen(config.server.port, () => Logger.info(`Server is running on port ${config.server.port}`));
+};
 
 initServer();
