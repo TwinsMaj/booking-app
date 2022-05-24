c9112cb542f1b143ff5ce658bd3913ccd83da924
Taiwo Maja <taiwom@klluster.com>
2022-05-24 11:54:01 +0300

    scaffold server

---
+++ knexfile.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+	development: {
+		client: "sqlite3",
+		connection: {
+			filename: "./dev.sqlite3",
+		},
+		migrations: {
+			tableName: 'migrations'
+		}
+	},
+};
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('watch me..');
+import express from 'express';
+import http from 'http'
+
+const app = express();
+
+const initServer = () => {
+	http.createServer(app).listen(8585, () => console.log(`Server started`))
+}
+
+initServer();
